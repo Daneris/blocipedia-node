b899dda83d3097457b833a1c2c274f0bc4173242
Daneris Cruz <daneriscruz@Daneriss-MacBook-Air.local>
2019-04-25 21:43:09 -0400

    didn't do the wiki database associations

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,6 +1,11 @@
+const staticRoutes = require("../routes/static");
+const wikiRoutes = require("../routes/wikis");
+
+
 module.exports = {
   init(app){
     const staticRoutes = require("../routes/static");
     app.use(staticRoutes);
+    app.use(wikiRoutes);
   }
 }
