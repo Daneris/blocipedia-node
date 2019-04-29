a4ef754a2d14d3ebd6185aacb99e9be529e8979c
Daneris Cruz <daneriscruz@Daneriss-MacBook-Air.local>
2019-04-29 18:28:47 -0400

    edits to scaffolding and deployments

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,7 +1,22 @@
 require("dotenv").config();
+const path = require("path");
+const viewsFolder = path.join(__dirname, "..", "views");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+const session = require("express-session");
+const flash = require("express-flash");
+const passportConfig = require("./passport-config");
+const logger = require("morgan");
+
 
 module.exports = {
-  init(){
+  init(app,express){
+
+    app.use(logger('dev'));
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use(expressValidator());
+    app.use(flash());
+
 
   }
 };
